f172dd17c6812477837a13ebdeaabc3bd01382cf
Wei Guangcheng <twksos@qq.com>
2013-08-20 17:29:50 +0800

    init xkx script repo

---
+++ utf82gbk.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import sys
+import os
+full_dir = os.path.dirname(os.path.realpath(__file__))
+ifile_dir = os.path.join(full_dir,'tins_utf8')
+ofile_dir = full_dir
+
+def utf8_to_gbk(file_name):
+	ifile_path = os.path.join(ifile_dir,file_name)
+	fi = open(ifile_path, "r")
+	utf8_str = fi.read()
+	fi.close()
+
+	gbk_str = utf8_str.decode('UTF-8').encode('GBK')
+
+	ofile_path = os.path.join(ofile_dir,file_name+'.tin')
+	fo = open(ofile_path, "w")
+	fo.write(gbk_str);
+	fo.close()
+
+for root, dirs, files in os.walk(ifile_dir):
+  for name in files:
+  	if name != '.DS_Store':
+  	  print 'transfer file: ' + name
+  	  utf8_to_gbk(name)
